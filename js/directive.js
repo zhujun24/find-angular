93252664c8cec4279e65f70e77d6c1708b89dc81
zhujun87654321 <zhujun87654321@gmail.com>
2015-05-27 20:21:28 +0800

    add low directive

--- js/directive.js
+++ js/directive.js
@@ -4,7 +4,38 @@ qianxun.directive('galleryDirective', function () {
     return {
         restrict: 'E',
         replace: true,
-        templateUrl: 'tpl/gallery.html'
+        templateUrl: 'tpl/gallery.html',
+        link: function () {
+            //预加载图片
+            $.preloadImages = function () {
+                for (var i = 0; i < arguments.length; i++)
+                    $("<img>").attr("src", arguments[i]);
+            };
+            //3秒后加载所有图片
+            $(document).delay(3000).queue(function () {
+                $.preloadImages("/images/2.jpg", "/images/3.jpg", "/images/4.jpg", "/images/5.jpg");
+            });
+
+            // 图片轮播
+            var num = 1;
+
+            function change() {
+                num++;
+                num = (num + 4) % 5 + 1;
+                $('#bg').fadeTo('slow', 0, function () {
+                    $(this).attr('src', 'images/' + num + '.jpg');
+                }).fadeTo('slow', 1);
+            }
+
+            var time1 = setInterval(change, 5000);
+
+            $('.jumbotron').mouseover(function () {
+                clearInterval(time1);
+            });
+            $('.jumbotron').mouseout(function () {
+                time1 = setInterval(change, 5000);
+            });
+        }
     }
 });
 
